27f382b9c34e81df7cfcb9f4c6c1e313f4443bb9
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2022-01-08 09:44:51 -0500

    publish by value, not ref

--- src/homie_impl.cpp
+++ src/homie_impl.cpp
@@ -29,7 +29,7 @@ void homie::Device::computePsk()
     }
 }
 
-void homie::Device::publish(Message &m)
+void homie::Device::publish(Message m)
 {
     mgos_mqtt_pub(m.topic.c_str(), m.payload.c_str(), m.payload.length(), m.qos, m.retained);
     LOG(LL_DEBUG, ("pub t=%s", m.topic.c_str()));
